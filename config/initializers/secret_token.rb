f25b01434dd1a736d9f709efc6145758525d66ec
jarvis <jarvis@newsnk.(none)>
2012-01-29 23:48:26 +0800

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guustock::Application.config.secret_token = '88294cbf4c36d4546507013d21d3f0177ec38f300c00ad5892db9313a7890749f59dcf577287682a8b7998a7399d93f1538ed69bca27da8ceb0ca071e463241e'
